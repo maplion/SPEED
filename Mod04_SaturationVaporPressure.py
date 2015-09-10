a0542c0bd215ffa6e27ca6f8719db87bc1fc8187
Ryan Dammrose <ryan@maplion.com>
2015-09-10 00:13:06 -0600

    Updates to Saturation Vapor Pressure calculation

--- Mod04_SaturationVaporPressure.py
+++ Mod04_SaturationVaporPressure.py
@@ -6,6 +6,20 @@ Created on Fri Sep 09 22:40:00 2015
 """
 
 import speedcalc
+import matplotlib.pyplot as plt
 
-sc_Pressure = speedcalc.Pressure(printFormula="true")
-sc_Pressure.saturationVaporPressure(10, resultPascal="kPa")
+sc_Pressure = speedcalc.Pressure()
+
+vaporPressureList = []
+temperatureList = []
+temperature = -15.0
+while temperature < 35.01:
+    vaporPressure = sc_Pressure.saturationVaporPressure(temperature, resultPascal="kPa")
+    print("T = {0} deg. C, esat = {1} kPa".format(temperature, round(vaporPressure, 2)))
+    vaporPressureList.append(round(vaporPressure, 2))
+    temperatureList.append(round(temperature, 2))
+    temperature += 0.01
+plt.plot(temperatureList, vaporPressureList)
+plt.xlabel("Temperature [Celsius Degrees]")
+plt.ylabel("Saturation vapor pressure (e*) [kPa]")
+plt.show()
